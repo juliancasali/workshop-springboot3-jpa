7ef4a180d2a18b68fe126abf1db30da30b548392
juliancasali <juliancasalii@gmail.com>
2022-12-18 00:00:40 -0300

    H2 database, test profile, JPA

--- src/main/java/com/example/projectspringboot/entities/User.java
+++ src/main/java/com/example/projectspringboot/entities/User.java
@@ -10,10 +10,17 @@ Basic entity checklist:
  Serializable
 */
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
