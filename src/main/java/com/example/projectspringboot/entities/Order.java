039c25d04e672a794e7317b8f21cffd209e22ad8
juliancasali <juliancasalii@gmail.com>
2022-12-19 22:32:03 -0300

    OrderStatus enum

--- src/main/java/com/example/projectspringboot/entities/Order.java
+++ src/main/java/com/example/projectspringboot/entities/Order.java
@@ -1,5 +1,6 @@
 package com.example.projectspringboot.entities;
 
+import com.example.projectspringboot.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -20,6 +21,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     // Associations (instantiate collections)
     @ManyToOne
     @JoinColumn(name = "cliente_id")
@@ -30,9 +33,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -50,6 +54,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
